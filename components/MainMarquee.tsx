7c5ce708e4e5d329f5dbed4f948cd30bba062b28
shiva kumar <98257507+Shivuduu19@users.noreply.github.com>
2025-03-25 13:42:31 +0530

    fix marquee angle

--- components/MainMarquee.tsx
+++ components/MainMarquee.tsx
@@ -38,8 +38,8 @@ let marqueeItems = [
 
 const MainMarquee = (props: Props) => {
   return (
-    <div className="w-screen h-[100px] flex items-center overflow-x-hidden my-9">
-      <div className="mx-auto h-[71px] min-w-full  -rotate-3 border border-primary-brown bg-primary-brown ">
+    <div className="w-screen h-[100px] -rotate-3 origin-bottom-left flex items-center overflow-x-hidden my-9">
+      <div className="mx-auto h-[71px] min-w-full   border border-primary-brown bg-primary-brown ">
         <div
           className="py-4 overflow-hidden flex flex-row items-center h-full relative w-full 
         "
